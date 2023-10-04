5ac34fa84890a104928e25807c9c87016ed89587
NavyDevilDoc <79477977+NavyDevilDoc@users.noreply.github.com>
2023-10-03 20:05:51 -0400

    Update Dogs_and_Cats_Image_Classifier.py
    Refactored version of the code. All updates have been incorporated and comments have been expanded.

--- Dogs_and_Cats_Image_Classifier.py
+++ Dogs_and_Cats_Image_Classifier.py
@@ -15,6 +15,11 @@ import shutil
 from random import sample
 
 '''
+# This section is how I set up my Google Colab environment. This was my first time trying
+# anything like this so if anyone knows how to do it better/more efficiently, don't hesitate
+# to let me know.
+
+# Mount a Google Drive 
 from google.colab import drive
 drive.mount('/content/gdrive')
 
@@ -30,7 +35,7 @@ drive.mount('/content/gdrive')
 !cp -r "/content/gdrive/MyDrive/Colab_Notebooks_2/PetImages/test/cats/"* /content/test/cats/
 !cp -r "/content/gdrive/MyDrive/Colab_Notebooks_2/PetImages/test/dogs/"* /content/test/dogs/
 
-# Creating validation folders and moving 400 random images from test to validation
+# Creating validation folders and moving 400 random cat/dog images from test to validation
 !mkdir -p /content/validation/cats
 !mkdir -p /content/validation/dogs
 
@@ -43,7 +48,7 @@ for animal in ['cats', 'dogs']:
         shutil.move(src_folder + image, dest_folder + image)
 '''
 
-
+# Convolutional block for the classifier
 def conv_block(x, filters, kernel_size, pool_size=(2, 2), dropout_rate=0.2, use_dropout=True):
     x = Conv2D(filters, kernel_size, kernel_initializer='he_normal', padding='same')(x)
     x = BatchNormalization()(x)
@@ -53,7 +58,10 @@ def conv_block(x, filters, kernel_size, pool_size=(2, 2), dropout_rate=0.2, use_
         x = Dropout(dropout_rate)(x)
     return x
 
-
+# Set the data augmentation parameters and create the training, validation, and testing generators
+# Note that if you're going to run this on your local machine, you'll need to change this line:
+# '/content/train'
+# to your file path
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     rotation_range=20,
@@ -61,42 +69,50 @@ train_datagen = ImageDataGenerator(
     height_shift_range=0.2,
     shear_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True
-)
+    horizontal_flip=True)
+
 train_generator = train_datagen.flow_from_directory(
     '/content/train',
     target_size=(200, 200),
     batch_size=32,
-    class_mode='binary'
-)
+    class_mode='binary')
+
 validation_datagen = ImageDataGenerator(rescale=1./255)
 validation_generator = validation_datagen.flow_from_directory(
     '/content/validation',
     target_size=(200, 200),
     batch_size=32,
-    class_mode='binary'
-)
+    class_mode='binary')
+
 test_datagen = ImageDataGenerator(rescale=1./255)
 test_generator = test_datagen.flow_from_directory(
     '/content/test',
     target_size=(200, 200),
     batch_size=32,
-    class_mode='binary'
-)
-# Counting training instances
+    class_mode='binary')
+
+# Check to ensure the data splitting went off as expected. This assumes that cats
+# are labeled as '0' and dogs are labeled as '1'. Generally speaking, I'm not the
+# best at this but after multiple checks against what's actually in my file path, 
+# everything was as it should be.
 train_cats = sum(train_generator.labels == 0)  # Assuming 0 is the label for 'cat'
 train_dogs = sum(train_generator.labels == 1)  # Assuming 1 is the label for 'dog'
 print(f"Training set has {train_cats} cat images and {train_dogs} dog images.")
-# Counting validation instances
-val_cats = sum(validation_generator.labels == 0)  # Assuming 0 is the label for 'cat'
-val_dogs = sum(validation_generator.labels == 1)  # Assuming 1 is the label for 'dog'
+
+val_cats = sum(validation_generator.labels == 0)  
+val_dogs = sum(validation_generator.labels == 1)  
 print(f"Validation set has {val_cats} cat images and {val_dogs} dog images.")
-# Counting test instances
-test_cats = sum(test_generator.labels == 0)  # Assuming 0 is the label for 'cat'
-test_dogs = sum(test_generator.labels == 1)  # Assuming 1 is the label for 'dog'
+
+test_cats = sum(test_generator.labels == 0)  
+test_dogs = sum(test_generator.labels == 1)  
 print(f"Test set has {test_cats} cat images and {test_dogs} dog images.")
 
-# Input layer
+'''
+I went with a multi-headed CNN architecture with varying kernel sizes. Compared
+to my initial model, which was a shallow CNN, test results increased from 45% to
+97.7% at 100 epochs. Note that since I used early stopping, I never saw more than
+85 epochs in a single run.
+'''
 input_layer = Input(shape=(200, 200, 3))
 
 # First head
@@ -120,7 +136,6 @@ x = conv_block(x, 128, (5, 5), use_dropout=False)
 x = conv_block(x, 64, (3, 3), use_dropout=False)
 x = conv_block(x, 32, (1,1), use_dropout=True)
 
-
 x = Flatten()(x)
 x = Dense(128, activation='relu', kernel_initializer='he_normal')(x)
 x = Dense(128, activation='relu', kernel_initializer='he_normal')(x)
@@ -138,8 +153,7 @@ history = model.fit(
     epochs=100,
     validation_data=validation_generator,
     validation_steps=validation_generator.samples // validation_generator.batch_size,
-    callbacks=[early_stop]
-)
+    callbacks=[early_stop])
 
 _, acc = model.evaluate_generator(test_generator,
                                   steps=len(test_generator),
@@ -156,7 +170,6 @@ plt.plot(history.history['loss'], label='loss')
 plt.plot(history.history['val_loss'], label='val_loss')
 plt.legend()
 plt.show()
-
 
 # Initialize empty arrays to store results
 all_labels = []
